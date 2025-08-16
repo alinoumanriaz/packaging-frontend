7a7c3335a42992d4201a5b77f03667519d661b3f
alinoumanriaz <alinoumanriaz@gmail.com>
2025-08-16 19:53:21 +0500

    some updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Features from "@/components/home-components/Features";
 import HeroBanner from "@/components/home-components/HeroBanner";
 import IndustryBoxesSection from "@/components/home-components/IndustryBoxesSection";
-import MaterialBoxesSection, {
-  IMaterial,
-} from "@/components/home-components/MaterialBoxesSection";
+import MaterialBoxesSection from "@/components/home-components/MaterialBoxesSection";
 import QuoteSection from "@/components/home-components/QuoteSection";
 import StyleBoxesSection from "@/components/home-components/StyleBoxesSection";
 import { GET_ALL_MATERIAL } from "@/graphql/queries/material.query";
@@ -21,7 +19,7 @@ export default async function Home() {
   return (
     <div className="flex-col space-y-16 md:space-y-24 mb-20">
       <HeroBanner />
-      <MaterialBoxesSection materials={data?.getAllMaterial as IMaterial[]} />
+      <MaterialBoxesSection materials={data?.getAllMaterial} />
       <IndustryBoxesSection />
       <StyleBoxesSection />
       <Features />
